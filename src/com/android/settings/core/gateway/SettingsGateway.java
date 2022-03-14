915ff7d19fba2e3e281f4b29f2056d495ab3d138
cjybyjk <cjybyjk@zjnu.edu.cn>
2022-03-15 03:08:51 +0800

    SoftAp: add client manager
    [jhonboy121]: adapted to A13
    
    Signed-off-by: cjybyjk <cjybyjk@zjnu.edu.cn>
    Change-Id: If9f0c1000ff4e7dd0b602a61299f1eb2c7608ac5
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -220,6 +220,8 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSettings2;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
+import ink.kscope.settings.wifi.tether.WifiTetherClientManager;
+
 public class SettingsGateway {
 
     /**
@@ -428,6 +430,7 @@ public class SettingsGateway {
             AppLocalePickerFragment.class.getName(),
             SupervisionDashboardFragment.class.getName(),
             BatteryShareSettings.class.getName(),
+            WifiTetherClientManager.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
